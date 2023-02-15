74a12f3e255f321b9b69010fc80751bd9330173c
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-02-15 13:43:48 +0400

    categories

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
 
   constructor() { }
 
@@ -16,5 +17,9 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
