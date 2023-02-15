026eeec308188ca139d94abc3f819c0f38be7388
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-02-15 15:01:44 +0400

    home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+
+const ROWS_HEIGHT:  { [id: number]: number} ={1: 400, 3: 335, 4: 350};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -8,7 +11,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  rowHeight = ROWS_HEIGHT[this.cols];
   category: string | undefined;
+
 
   constructor() { }
 
@@ -17,6 +22,7 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum;
+    this.rowHeight = ROWS_HEIGHT[this.cols];
   }
 
   onShowCategory(newCategory: string): void {
