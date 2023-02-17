358a541df2213e0ff9872fdd77ccea0e563cb33e
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-02-17 18:27:51 +0400

    product add to cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { ProductsHeaderComponent } from './pages/home/components/products-header
 import { FiltersComponent } from './pages/home/components/filters/filters.component';
 import { ProductBoxComponent } from './pages/home/components/product-box/product-box.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CartService } from './services/cart.service';
 
 
 @NgModule({
@@ -56,7 +57,7 @@ import { CartComponent } from './pages/cart/cart.component';
     BrowserAnimationsModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -7,10 +8,24 @@ import { Component, Input, OnInit } from '@angular/core';
 export class ProductBoxComponent implements OnInit {
 
   @Input() fullWidthMode = false;
+  product: Product | undefined = {
+    id: 1,
+    title: 'boot',
+    price: 3,
+    category: 'shoes',
+    description: 'test',
+    image: 'image',
+  }
+
+  @Output() addToCart = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddToCart(): void {
+    this.addToCart.emit(this.product);
   }
 
 }
