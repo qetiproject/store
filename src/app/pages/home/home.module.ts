d2c598e120eb8953f22d3791521cd6fd16411fa5
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-02-20 16:46:47 +0400

    clean modules

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomeComponent } from './home.component';
-import { ProductsHeaderComponent } from './components/products-header/products-header.component';
-import { FiltersComponent } from './components/filters/filters.component';
-import { ProductBoxComponent } from './components/product-box/product-box.component';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatButtonModule } from '@angular/material/button';
@@ -18,8 +14,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
-
-
+import { FiltersComponent, HomeComponent, ProductBoxComponent, ProductsHeaderComponent } from './index';
 
 @NgModule({
   declarations: [
