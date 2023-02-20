74390c6df90336e8c4db687f8648d4db705850a0
Qeti Khetsuriani <ketideveloper@gmail.com>
2023-02-20 10:49:27 +0400

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -58,6 +58,7 @@ export class HomeComponent implements OnInit, OnDestroy {
       })
   }
 
+  
   ngOnDestroy(): void {
     if(this.productsSubscription) {
       this.productsSubscription.unsubscribe();
